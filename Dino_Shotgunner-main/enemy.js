8abbde8119731cb4bb53849a248c27152bfd0032
ibizaa2 <111568546+ibizaa2@users.noreply.github.com>
2026-04-24 00:08:40 +0300

    Update enemy.js

--- Dino_Shotgunner-main/enemy.js
+++ Dino_Shotgunner-main/enemy.js
@@ -45,7 +45,7 @@ class Bird {
         this.damage = 5;
 
         this.img = new Image();
-        this.img.src = ".Images/Bird.png";
+        this.img.src = "./Images/Bird.png";
 
         this.img.onload = () => {
             this.width = this.img.width * this.scale;
@@ -141,13 +141,13 @@ class Cactus {
         this.needleScale = 2;
         this.needles = []
         this.needlesImg = new Image();
-        this.needlesImg.src = ".Images/Needle projectile.png"
+        this.needlesImg.src = "./Images/Needle projectile.png"
 
         this.grenadeboom = new Audio();
-        this.grenadeboom.src = ".Audio/Grenade.Boom.wav"
+        this.grenadeboom.src = "./Audio/Grenade.Boom.wav"
 
         this.grenadeImg = new Image()
-        this.grenadeImg.src = ".Images/Cactus grenade.png"
+        this.grenadeImg.src = "./Images/Cactus grenade.png"
 
        this.launchgrenade = false;
 
